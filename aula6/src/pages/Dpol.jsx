e9da213ad7a48a2ba750bae484e4817176335e11
João Vitor <143559938+JANzxz@users.noreply.github.com>
2025-04-30 21:12:56 -0300

    Add files via upload

--- aula6/src/pages/Dpol.jsx
+++ aula6/src/pages/Dpol.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Dpol = () => {
+
+  const [dpoLgpd, SetDpoLgpd] = useState([])
+  
+  useEffect(() => {
+    axios.get("http://localhost:3000/dpolgpd")//Request
+    .then(response => SetDpoLgpd(response.data))//response(resposta do portugues)
+    .catch(error => console.error("Erro ao carregar a lista de noticias: ", error))
+  },[])
+  
   return (
-    <div>
-        <h3>Essa e a pagina Dpol</h3>
-    </div>
+  <div>
+    <h1>DpoLgpd</h1>
+    <ul>
+      {
+        dpoLgpd.map((dpolgpd) => (
+          <li key={dpolgpd.id}>
+            <h2>{dpolgpd.titulo}</h2>
+            <h3>{dpolgpd.texto}</h3>
+          </li>
+        ))
+      }
+    </ul>
+</div>
   )
 }
 
--- aula6/src/pages/Noticias.jsx
+++ aula6/src/pages/Noticias.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Noticias = () => {
+
+  const [noticias, SetNoticias] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/noticias")//Request
+    .then(response => SetNoticias(response.data))//response(resposta do portugues)
+    .catch(error => console.error("Erro ao carregar a lista de noticias: ", error))
+  },[])
+
   return (
     <div>
-        <h3><h3>Essa e a pagina de Noticias</h3></h3>
+        <h1>Notícias</h1>
+        <ul>
+          {
+            noticias.map((noticia) => (
+              <li key={noticia.id}>
+                <h2>{noticia.titulo}</h2>
+                <h3>{noticia.texto}</h3>
+              </li>
+            ))
+          }
+        </ul>
+
     </div>
   )
 }
